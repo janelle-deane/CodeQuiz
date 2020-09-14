ac279b4430c391be7599626457b149f269185e6a
JD <janelle.m.deane@gmail.com>
2020-09-13 21:36:59 -0700

    structured index

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 console.log("linked")
 
+
+// Make Question Array
+var questionArray= [
+
+{question: "A stellate object has what shape?",
+ answers: ["A: Spiral", "B: Star", "C: Conical", "D: Hexagon"]
+},{
+question: "What is a Vitrine?",
+answers: ["A: Fossilized log", "B: A type of vitamin", "C: A glass display cabinet", "D: A Norse women warrior"]
+},{
+question: "What does Schmuck in German literally mean?", 
+answers: ["A: Jewellery", "B: A silly person","C: A surname", "D: A shoe"]
+},{
+question: "What is an aglet?",
+answers: ["A: The plastic tip on the end of your shoelace", "B: A type of rock", "C: A small silver coin", "D: An acute angle"]
+},{
+question: "What is a Zarf?"
+answers: "A: Bridge pillar", "B: A zebra calf", "C:A zebra's barf", "D: Coffee cup sleeve"]
+}]
+
+
+
 // Questions are hidden 
 // Questions changes when clicked on a answer
 // Wrong or Correct are Shown when approirate response is choosen
